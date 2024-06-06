de8fc20f603834169ed6f0f692cb600f0585a711
Aline Pinhelli <aline.pinhelli@grupoboticario.com.br>
2024-06-06 17:24:59 -0300

    all

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { FiLogIn, FiLogOut, FiShoppingCart } from "react-icons/fi";
+import { Cart } from "../Cart/Cart";
+
+import * as S from "./styles";
+
+export const Header: React.FC = () => {
+  const [showCart, setShowCart] = useState(false);
+  const isLogged = false;
+
+  return (
+    <S.StyleHeader>
+      <S.Wrapper>
+        <S.HeaderTitle> MyShop.</S.HeaderTitle>
+        <S.ButtonsWrapper>
+          <S.AuthButton isLogged={isLogged}>
+            {isLogged ? "Logout" : "Login"}
+            {isLogged ? <FiLogOut /> : <FiLogIn />}
+          </S.AuthButton>
+          <S.CartButton onClick={() => setShowCart(!showCart)}>
+            Carrinho
+            <FiShoppingCart />
+          </S.CartButton>
+        </S.ButtonsWrapper>
+      </S.Wrapper>
+
+      <Cart showCart={showCart} />
+    </S.StyleHeader>
+  );
+};
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+export const StyleHeader = styled.header`
+  background-color: navy;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1240px;
+  margin: 0 auto;
+  padding: 0 2rem;
+  height: 60px;
+`;
+
+export const HeaderTitle = styled.h1`
+  color: white;
+  font-size: 1.6rem;
+  font-weight: 500;
+`;
+
+export const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+interface AuthButtonProps {
+  isLogged: boolean;
+}
+
+export const AuthButton = styled.button<AuthButtonProps>`
+  border: none;
+  border-radius: 5px;
+  height: 30px;
+  padding: 0 1rem;
+  background-color: ${(props) => (props.isLogged ? "red" : "green")};
+  color: white;
+  font-size: 0.75rem;
+
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+
+  svg {
+    font-size: 0.7rem;
+  }
+`;
+
+export const CartButton = styled.button`
+  border: none;
+  border-radius: 5px;
+  height: 30px;
+  padding: 0 1rem;
+  background-color: violet;
+  color: black;
+  font-size: 0.75rem;
+
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+
+  svg {
+    font-size: 0.7rem;
+  }
+`;
